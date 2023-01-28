15acaa2edd6a5c65b098ed6bbf5ed8a21bcb2e98
Lucas Barboza Costa <122161095+Lucasbc47@users.noreply.github.com>
2023-01-28 17:50:45 -0300

    Add files via upload

---
+++ exs/012.py
@@ -0,0 +1,7 @@
+# Tabuada com while
+n = int(input("Digite um número:\n"))
+contador = 0
+
+while contador < 10:
+    contador += 1
+    print(f'{n} x {contador} = {contador * n}')
---
+++ exs/013.py
@@ -0,0 +1,5 @@
+# Tabuada com for
+n = int(input("Digite um número:\n"))
+
+for x in range(1, 11):
+    print(f'{n}x{x} = {n * x}')
